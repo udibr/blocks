7f4cf9bbec92dc3b9f232bd6f207fdb1a4a41164
Ehud Ben-Reuven <udi@benreuven.com>
2015-05-25 09:24:46 -0400

    undo use_bias

--- blocks/bricks/parallel.py
+++ blocks/bricks/parallel.py
@@ -58,11 +58,11 @@ class Parallel(Initializable):
     """
     @lazy(allocation=['input_dims', 'input_dims', 'output_dims'])
     def __init__(self, input_names, input_dims, output_dims,
-                 prototype=None, child_prefix=None, use_bias=False,
+                 prototype=None, child_prefix=None,
                  **kwargs):
         super(Parallel, self).__init__(**kwargs)
         if not prototype:
-            prototype = Linear(use_bias=use_bias)
+            prototype = Linear(use_bias=False)
         if not child_prefix:
             child_prefix = "transform"
 
